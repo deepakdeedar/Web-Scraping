8cd9d4fbe13209198026be9af15d2b25819280e5
Deepak Deedar Singla <47693198+deepakdeedar@users.noreply.github.com>
2020-09-21 16:52:45 +0530

    final commit

--- scrap.py
+++ scrap.py
@@ -1,2 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
+
+res = requests.get('https://news.ycombinator.com/news')
+soup = BeautifulSoup(res.text, 'html.parser')
+links = soup.select('.storylink')
+subtext = soup.select('.subtext')
+
+
+def custom_hn(links, subtext):
+    hn = []
+    for idx, item in enumerate(links):
+        title = links[idx].getText()
+        href = links[idx].get('href')
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({'title': title, 'href': href, 'votes': points})
+    return story_with_votes(hn)
+
+
+def story_with_votes(hn):
+    return sorted(hn, key=lambda k: k['votes'], reverse=True)
+
+
+pprint.pprint(custom_hn(links, subtext))
